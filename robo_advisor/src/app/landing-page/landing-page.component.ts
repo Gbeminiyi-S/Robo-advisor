352878a5e7a04bb4b9b5cdb8ecd1f45c9a0aff38
Tonyrealzy <umehobiarinze2@gmail.com>
2024-02-22 11:45:47 +0100

    Created components for the navbar, footer, landing-page and the forms-page

--- robo_advisor/src/app/landing-page/landing-page.component.ts
+++ robo_advisor/src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
-  selector: 'app-landing-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+    selector: 'app-landing-page',
+    standalone: true,
+    templateUrl: './landing-page.component.html',
+    styleUrl: './landing-page.component.css',
+    imports: [FooterComponent, NavbarComponent]
 })
 export class LandingPageComponent {
   title = 'Landing Page works!';
