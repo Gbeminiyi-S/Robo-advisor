8d456113a51384cf7e73be1a108e89d9740310ef
ig-matrix <omomaighawosa440@gmail.com>
2024-02-23 14:10:15 +0100

    routed the components to matching routes and restructured components

--- robo_advisor/src/app/app.routes.ts
+++ robo_advisor/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { FormsPageComponent } from './forms-page/forms-page.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { FormComponent } from './components/form/form.component';
 
 export const routes: Routes = [
-    {path: '', component: LandingPageComponent},
-    {path: 'formspage', component: FormsPageComponent}
+  { path: '', component: LandingPageComponent },
+  { path: 'formspage',title:'user form', component: FormComponent },
 ];
--- robo_advisor/src/app/components/navbar/navbar.component.ts
+++ robo_advisor/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
