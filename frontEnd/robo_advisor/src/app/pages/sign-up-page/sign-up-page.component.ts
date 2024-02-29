549895f5b6a4b0efec31eddede63ff33f0698323
ig-matrix <omomaighawosa440@gmail.com>
2024-02-29 11:48:33 +0100

    send user sign up data to api

--- frontEnd/robo_advisor/src/app/pages/sign-up-page/sign-up-page.component.ts
+++ frontEnd/robo_advisor/src/app/pages/sign-up-page/sign-up-page.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { SignUpService } from '../../services/sign-up/sign-up.service';
+import { response } from 'express';
+import { error } from 'console';
 
 @Component({
   selector: 'app-sign-up-page',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, ReactiveFormsModule],
   templateUrl: './sign-up-page.component.html',
-  styleUrl: './sign-up-page.component.css'
+  styleUrl: './sign-up-page.component.css',
 })
 export class SignUpPageComponent {
+  isLoading: boolean= false;
+  signUpForm: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    email: new FormControl(''),
+  });
 
+  constructor(private signUpService: SignUpService) {}
+
+  onSubmit(): void {
+    this.isLoading=true;
+    if (this.signUpForm.valid) {
+      const signUpData = this.signUpForm.value;
+      this.signUpService.signUp(signUpData).subscribe(
+        (response) => {
+          this.isLoading=false;
+          console.log(response.message);
+        },
+        (error) => {
+          this.isLoading=false;
+          console.log(error);
+        },
+        );
+      } else {
+        console.error('Form is invalid');
+        this.isLoading=false;
+    }
+  }
 }
