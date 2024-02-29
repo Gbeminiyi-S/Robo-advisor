586257eee4b3cff5f8403427f1ec8304c95a911a
ig-matrix <omomaighawosa440@gmail.com>
2024-02-29 08:25:36 +0100

    linked sign in and sign up

--- frontEnd/robo_advisor/src/app/pages/sign-up-page/sign-up-page.component.ts
+++ frontEnd/robo_advisor/src/app/pages/sign-up-page/sign-up-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sign-up-page.component.html',
   styleUrl: './sign-up-page.component.css'
 })
