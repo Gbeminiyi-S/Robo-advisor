703245c859b8b3a9380a42d66bb9351ce5a9f616
Moongenevieve <fsamson5@gmail.com>
2024-02-29 12:57:45 +0100

    Added the about us page

--- frontEnd/robo_advisor/src/app/app.routes.ts
+++ frontEnd/robo_advisor/src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { Routes } from '@angular/router';
 import { FormComponent } from './pages/form/form.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
@@ -7,10 +8,11 @@ import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component
 import { Error404Component } from './pages/error404/error404.component';
 
 export const routes: Routes = [
-  { path: '', title: 'Landing Page', component: LandingPageComponent },
+  { path: '', title: 'Landing Page', component: AboutUsComponent },
   { path: 'login', title: 'Log-in Page', component: LoginpageComponent },
   { path: 'signup', title: 'Sign-up Page', component: SignUpPageComponent },
   { path: 'formspage', title: 'User form', component: FormComponent },
   { path: 'homepage', title: 'Homepage', component: HomepageComponent },
+  { path: 'aboutuspage', title: 'AboutUspage', component: AboutUsComponent },
   { path: '**', title: 'error404', component: Error404Component },
 ];
