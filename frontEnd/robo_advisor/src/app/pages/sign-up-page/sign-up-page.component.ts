0c28ffce3f7f0207b1c3dffb1721b6987fa6b7be
Tonyrealzy <umehobiarinze2@gmail.com>
2024-03-01 16:20:02 +0100

    Changes made as a result of pulling from dev

--- frontEnd/robo_advisor/src/app/pages/sign-up-page/sign-up-page.component.ts
+++ frontEnd/robo_advisor/src/app/pages/sign-up-page/sign-up-page.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { SignUpService } from '../../services/sign-up/sign-up.service';
+import { response } from 'express';
+import { error } from 'console';
 
 @Component({
   selector: 'app-sign-up-page',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, CommonModule, FormsModule],
+  imports: [RouterLink, RouterLinkActive, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './sign-up-page.component.html',
   styleUrl: './sign-up-page.component.css',
 })
@@ -17,6 +20,36 @@ export class SignUpPageComponent {
   password: string = '';
   // this stores the confirmation password value
   confirmPassword: string = '';
+
+  isLoading: boolean= false;
+  signUpForm: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    email: new FormControl(''),
+  });
+
+  constructor(private signUpService: SignUpService, private router: Router) {}
+
+  onSubmit(): void {
+    this.isLoading=true;
+    if (this.signUpForm.valid) {
+      const signUpData = this.signUpForm.value;
+      this.signUpService.signUp(signUpData).subscribe(
+        (response) => {
+          this.isLoading=false;
+          console.log(response.message);
+          this.router.navigate(['/login'])
+        },
+        (error) => {
+          this.isLoading=false;
+          console.log(error);
+        },
+        );
+      } else {
+        console.error('Form is invalid');
+        this.isLoading=false;
+    }
+  }
 
   togglePasswordVisibility(field: 'password' | 'confirmPassword'): void {
     this.switchIcon = !this.switchIcon;    
