4b49c86c1f39277e76b32b53a6651ec1188c8982
ig-matrix <omomaighawosa440@gmail.com>
2024-03-01 22:55:25 +0100

    add show eye icon to login page

--- frontEnd/robo_advisor/src/app/pages/loginpage/loginpage.component.ts
+++ frontEnd/robo_advisor/src/app/pages/loginpage/loginpage.component.ts
@@ -2,14 +2,19 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { LoginService } from '../../services/login/login.service';
-import { response } from 'express';
-import { error } from 'console';
 import { UserDetailsService } from '../../services/user-details/user-details.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-loginpage',
   standalone: true,
-  imports: [RouterLinkActive, RouterLink, ReactiveFormsModule],
+  imports: [
+    RouterLinkActive,
+    RouterLink,
+    ReactiveFormsModule,
+    FontAwesomeModule,
+  ],
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.css',
 })
@@ -21,9 +26,11 @@ export class LoginpageComponent {
     email: new FormControl(''),
     password: new FormControl(''),
   });
-  
-  onShowPassword(): void {
-    this.isShowPassword=!this.isShowPassword
+  faEyeSlash = faEyeSlash;
+  faEye = faEye;
+
+  toggleShowPassword(): void {
+    this.isShowPassword = !this.isShowPassword;
   }
 
   constructor(
