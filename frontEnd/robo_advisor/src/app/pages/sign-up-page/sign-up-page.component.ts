9f130a7661c83ed6c83d3479b27484a08f7b221e
Tonyrealzy <umehobiarinze2@gmail.com>
2024-03-01 15:59:08 +0100

    Changes made after pulling from dev

--- frontEnd/robo_advisor/src/app/pages/sign-up-page/sign-up-page.component.ts
+++ frontEnd/robo_advisor/src/app/pages/sign-up-page/sign-up-page.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { FormsModule, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink, RouterLinkActive } from '@angular/router';
-import { SignUpService } from '../../services/sign-up/sign-up.service';
-import { response } from 'express';
-import { error } from 'console';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up-page',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [RouterLink, RouterLinkActive, CommonModule, FormsModule],
   templateUrl: './sign-up-page.component.html',
   styleUrl: './sign-up-page.component.css',
 })
@@ -20,13 +17,6 @@ export class SignUpPageComponent {
   password: string = '';
   // this stores the confirmation password value
   confirmPassword: string = '';
-  isLoading: boolean= false;
-
-  signUpForm: FormGroup = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-    email: new FormControl('')
-  });
 
   togglePasswordVisibility(field: 'password' | 'confirmPassword'): void {
     this.switchIcon = !this.switchIcon;    
@@ -35,33 +25,10 @@ export class SignUpPageComponent {
     if (field === 'password') {
         this.showPassword = !this.showPassword;
     }
-  }
+}
 
   // to see if the passwords match...
   confirmPasswordMatch(): boolean {
     return this.password === this.confirmPassword;
-  }
-
-  constructor(private signUpService: SignUpService, private router: Router) {}
-
-  onSubmit(): void {
-    this.isLoading=true;
-    if (this.signUpForm.valid) {
-      const signUpData = this.signUpForm.value;
-      this.signUpService.signUp(signUpData).subscribe(
-        (response) => {
-          this.isLoading=false;
-          console.log(response.message);
-          this.router.navigate(['/login'])
-        },
-        (error) => {
-          this.isLoading=false;
-          console.log(error);
-        },
-        );
-      } else {
-        console.error('Form is invalid');
-        this.isLoading=false;
-    }
   }
 } 
