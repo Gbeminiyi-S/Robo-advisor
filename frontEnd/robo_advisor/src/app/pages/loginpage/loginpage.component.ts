caa7353112c229a037f203c341160a7d11e650f6
Tonyrealzy <umehobiarinze2@gmail.com>
2024-03-01 13:57:23 +0100

    Fit/freshchanges made to the login and sign-up pages, alongside implementation of "show/hide password" today/ROB-5

--- frontEnd/robo_advisor/src/app/pages/loginpage/loginpage.component.ts
+++ frontEnd/robo_advisor/src/app/pages/loginpage/loginpage.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-loginpage',
   standalone: true,
-  imports: [RouterLinkActive, RouterLink],
+  imports: [RouterLinkActive, RouterLink, CommonModule, FormsModule],
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.css'
 })
 export class LoginpageComponent {
+  switchIcon: boolean = true;
+  showPassword: boolean = true;
+  // this takes password value
+  password: string = '';
+
+
+  togglePasswordVisibility(): void {
+    this.switchIcon = !this.switchIcon
+    this.showPassword = !this.showPassword
+    
+    // Toggle the showPassword only for the specified field
+    // if (field === 'password') {
+    //     this.showPassword = !this.showPassword;
+    // }
+}
 
 }
--- frontEnd/robo_advisor/src/app/pages/sign-up-page/sign-up-page.component.ts
+++ frontEnd/robo_advisor/src/app/pages/sign-up-page/sign-up-page.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up-page',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule, FormsModule],
   templateUrl: './sign-up-page.component.html',
   styleUrl: './sign-up-page.component.css'
 })
 export class SignUpPageComponent {
+  switchIcon: boolean = true;
+  showPassword: boolean = true;
+  // this takes password value
+  password: string = '';
+  // this stores the confirmation password value
+  confirmPassword: string = '';
 
+  togglePasswordVisibility(field: 'password' | 'confirmPassword'): void {
+    this.switchIcon = !this.switchIcon;    
+
+    // Toggle the showPassword only for the specified field
+    if (field === 'password') {
+        this.showPassword = !this.showPassword;
+    }
 }
+
+  // to see if the passwords match...
+  confirmPasswordMatch(): boolean {
+    return this.password === this.confirmPassword;
+  }
+} 
