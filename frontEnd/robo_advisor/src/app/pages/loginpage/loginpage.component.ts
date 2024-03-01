90722e758fe33e16eb01d9a87b49210452e1741c
Tonyrealzy <umehobiarinze2@gmail.com>
2024-03-01 15:32:29 +0100

    Fit/freshchanges made to the login and sign-up pages, alongside implementation of "show/hide password" today/ROB-5

--- frontEnd/robo_advisor/src/app/pages/loginpage/loginpage.component.ts
+++ frontEnd/robo_advisor/src/app/pages/loginpage/loginpage.component.ts
@@ -1,30 +1,63 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { FormsModule,  FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { LoginService } from '../../services/login/login.service';
+import { response } from 'express';
+import { error } from 'console';
+import { UserDetailsService } from '../../services/user-details/user-details.service';
+
 
 @Component({
   selector: 'app-loginpage',
   standalone: true,
-  imports: [RouterLinkActive, RouterLink, CommonModule, FormsModule],
+  imports: [RouterLinkActive, RouterLink, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.css'
 })
 export class LoginpageComponent {
   switchIcon: boolean = true;
   showPassword: boolean = true;
+  isLoading: boolean = false;
   // this takes password value
   password: string = '';
+
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
 
   togglePasswordVisibility(): void {
     this.switchIcon = !this.switchIcon
     this.showPassword = !this.showPassword
-    
-    // Toggle the showPassword only for the specified field
-    // if (field === 'password') {
-    //     this.showPassword = !this.showPassword;
-    // }
-}
+  }
 
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private userDetails: UserDetailsService,
+  ) {}
+
+  onSubmit(): void {
+    this.isLoading = true;
+    if (this.loginForm.valid) {
+      const loginData = this.loginForm.value;
+      this.loginService.login(loginData).subscribe(
+        (response) => {
+          this.isLoading = false;
+          this.userDetails.setUsername(response.username);
+          this.router.navigate(['/homepage']);
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+          this.isLoading = false;
+        },
+      );
+    } else {
+      this.isLoading = false;
+      console.log('Invalid form');
+    }
+  }
 }
