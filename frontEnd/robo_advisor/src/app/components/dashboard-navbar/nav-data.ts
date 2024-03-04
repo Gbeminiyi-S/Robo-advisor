c97082a1d862cd5fc12db68c18f6e481703c2208
Moongenevieve <fsamson5@gmail.com>
2024-03-04 09:49:20 +0100

    feat/aboutusandcontactuspage/ROB-10

---
+++ frontEnd/robo_advisor/src/app/components/dashboard-navbar/nav-data.ts
@@ -0,0 +1,14 @@
+import { RouterLink } from '@angular/router';
+export const navbarData = [
+{
+    RouterLink: 'dashboard',
+    icon: 'fas fa-border-all',
+    label: 'Dashboard',
+},
+
+{
+    RouterLink: 'dashboard',
+    icon: 'fas fa-border-all',
+    label: 'Dashboard',
+}
+];
