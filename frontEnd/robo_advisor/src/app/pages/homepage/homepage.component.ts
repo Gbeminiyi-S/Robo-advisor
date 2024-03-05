6cf402892774573b02ebed9b7a52783ae97b2c13
Moongenevieve <fsamson5@gmail.com>
2024-03-05 18:41:59 +0100

    redesign of the dashbaord

--- frontEnd/robo_advisor/src/app/pages/homepage/homepage.component.ts
+++ frontEnd/robo_advisor/src/app/pages/homepage/homepage.component.ts
@@ -3,20 +3,32 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { UserDetailsService } from '../../services/user-details/user-details.service';
+import { BehaviorSubject } from 'rxjs';
+import { CommonModule, NgClass } from '@angular/common';
+
+
+
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [FooterComponent, NavbarComponent, RouterLink, RouterLinkActive],
+  imports: [FooterComponent, NavbarComponent, RouterLink, RouterLinkActive, NgClass, CommonModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent implements OnInit {
   username: string | null = null;
+  isLoggedIn$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor(private userDetails: UserDetailsService){}
 
   ngOnInit(): void {
     this.username= this.userDetails.getUsername();
+    this.userDetails.isLoggedIn.subscribe((loggedIn) => {
+      this.isLoggedIn$.next(loggedIn);
+    });
+  }
+  logOut(): void {
+    this.userDetails.clearUsername();
   }
 }
