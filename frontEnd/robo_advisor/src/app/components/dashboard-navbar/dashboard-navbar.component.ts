641fe0f08a118c9cc9cec898362a5503f40f910c
Tonyrealzy <umehobiarinze2@gmail.com>
2024-03-05 09:27:31 +0100

    Working on the login status

--- frontEnd/robo_advisor/src/app/components/dashboard-navbar/dashboard-navbar.component.ts
+++ frontEnd/robo_advisor/src/app/components/dashboard-navbar/dashboard-navbar.component.ts
@@ -5,52 +5,63 @@ import { navbarData } from './nav-data';
 import { UserDetailsService } from '../../services/user-details/user-details.service';
 import { BehaviorSubject } from 'rxjs';
 
-
 interface SideNavToggle {
   screenWidth: number;
   collapsed: boolean;
 }
-
-
 
 @Component({
   selector: 'app-dashboard-navbar',
   standalone: true,
   imports: [CommonModule, RouterLink, NgClass, RouterLinkActive],
   templateUrl: './dashboard-navbar.component.html',
-  styleUrl: './dashboard-navbar.component.css'
+  styleUrl: './dashboard-navbar.component.css',
 })
 export class DashboardNavbarComponent {
   @Output() onToggleSiddeNav: EventEmitter<SideNavToggle> = new EventEmitter();
-collapsed = false;
-showLabel = true;
-screenWidth = 0;
-navData = navbarData;
-isLoggedIn$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  collapsed = false;
+  showLabel = true;
+  screenWidth = 0;
+  navData = navbarData;
+  isLoggedIn$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
+  constructor(private userDetails: UserDetailsService) {}
 
-toggleCollapse(): void {
-  this.collapsed = !this.collapsed;
-  this.onToggleSiddeNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
-}
+  @HostListener('document:click', ['$event'])
+  ngOnInit(): void {
+    // adding methodology for making the user stay logged-in, on refreshing...
 
-closeSidenav(): void {
-  this.collapsed = false;
-  this.onToggleSiddeNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
-}
+    // to check if the user ws previously logged in...
+    const storedLoginStatus = localStorage.getItem('isLoggedIn');
+    const previousLoginStatus = storedLoginStatus
+      ? JSON.parse(storedLoginStatus)
+      : false;
 
+    // Set the initial login status
+    this.isLoggedIn$.next(previousLoginStatus);
 
-constructor(private userDetails: UserDetailsService) {}
-@HostListener('document:click', ['$event'])
+    // Subscribe to updates in the login status
+    this.userDetails.isLoggedIn.subscribe((loggedIn) => {
+      this.isLoggedIn$.next(loggedIn);
+      localStorage.setItem('isLoggedIn', JSON.stringify(loggedIn));
+    });
+  }
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSiddeNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
 
-ngOnInit(): void {
-  this.userDetails.isLoggedIn.subscribe((loggedIn) => {
-    this.isLoggedIn$.next(loggedIn);
-  });
-}
-logOut(): void {
-  this.userDetails.clearUsername();
-}
-
-
+  closeSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSiddeNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
+  logOut(): void {
+    this.userDetails.clearUsername();
+  }
 }
