c26bfeee53082f14aa0b241a063378f3867e0f75
ig-matrix <omomaighawosa440@gmail.com>
2024-03-06 09:41:46 +0100

    add modal, still not properly steezed

--- frontEnd/robo_advisor/src/app/components/dashboardComponents/modal/modal.component.ts
+++ frontEnd/robo_advisor/src/app/components/dashboardComponents/modal/modal.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FinancialProduct } from '../../../models/interfaces/ResponseList';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
   imports: [],
   templateUrl: './modal.component.html',
-  styleUrl: './modal.component.css'
+  styleUrl: './modal.component.css',
 })
 export class ModalComponent {
+  @Input() item!: FinancialProduct;
+  @Output() closeModal = new EventEmitter<void>();
 
+  onCloseModal(): void {
+    this.closeModal.emit();
+  }
+
+  stopPropagation(event: MouseEvent) {
+    event.stopPropagation();
+  }
 }
