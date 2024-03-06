4068ab71c7f1d970c7447270811b28ad04298056
Tonyrealzy <umehobiarinze2@gmail.com>
2024-03-06 07:47:35 +0100

    Latest changes based on Route protection

--- frontEnd/robo_advisor/src/app/app.config.server.ts
+++ frontEnd/robo_advisor/src/app/app.config.server.ts
@@ -3,16 +3,12 @@ import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthGuardService } from './services/auth_guard/auth-guard.service';
-import { AuthService } from './services/auth/auth.service';
-import { AuthInterceptor } from './services/auth_interceptor/auth.interceptor';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
     HttpClientModule,
     AuthGuardService,
-    AuthService,
-    AuthInterceptor
   ]
 };
 
