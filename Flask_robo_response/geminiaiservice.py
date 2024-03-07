60c89ebcca23bb96ccb4f69e5742242dff8a64a3
Tonyrealzy <umehobiarinze2@gmail.com>
2024-03-07 11:36:14 +0100

    Final Commit made

--- Flask_robo_response/geminiaiservice.py
+++ Flask_robo_response/geminiaiservice.py
@@ -1,66 +1,147 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 from services import *
 
 import os
-import google.generativeai as genai # importing dependencies for the AI service
-
-
-
-app = Flask(__name__)
-
+import google.generativeai as genai  # importing dependencies for the AI service
 value = os.getenv('GOOGLE_API_KEY')
+
 
 genai.configure(api_key=value)
 
 model = genai.GenerativeModel('gemini-pro')
 
+
+app = Flask(__name__)
+
+# capture and store the data received from post request
+# Configure CORS globally for all routes
+# Configure CORS globally to allow all headers (including 'Content-Type')
+# CORS(app, supports_credentials=False, origins="*", allow_headers="*", methods="*")
+
+# CORS(app, resources={r"/*": {"origins": "*"}})
+# CORS configuration
+cors = CORS(
+    app,
+    resources={r"/*": {"origins": "*"}},
+    supports_credentials=True,
+    allow_headers=["Content-Type"],
+    expose_headers=["Content-Type"],
+)
+
+# Your routes and other configurations go here
+# For example:
+
+
 # Store data in memory for the example
 data_store = {
-    "age": "", 
-    "location": "", 
-    "investment_skill_level": "", 
-    "investment_purpose": "", 
-    "investment_horizon": "",
-    "risk_tolerance": "",
+    "age": "",
+    "location": "",
+    "investmentKnowledge": "",
+    "investmentPurpose": "",
+    "investmentHorizon": "",
+    "riskTolerance": "",
     "amount": "",
     "currency": ""
 }
 
+response_store = {}
+
+def get_currency_symbol(currency):
+    match currency:
+        case "Dollar":
+            return "$"
+        case "Naira":
+            return "₦"
+        case "Cedi":
+            return "₵"
+        case "Pound":
+            return "£"
+        case "Euro":
+            return "€"
 
 
-# fucntion to calculate future value of expected return => FV = P( 1 + r)^t
+# function to calculate future value of expected return => FV = P( 1 + r)^t
+
+# Arinze commented this out...
 def calculate_return(response, data_store):
-    print(response.keys())
+    
+    from decimal import Decimal, getcontext
+    # print(response.keys())
     try:
         composition = response['composition']
         composition = float(composition.split("%")[0])
     except KeyError:
-        print(response)
+        # print(response)
         return response
 
-    expected_return = response['expected_return']
+    expected_return = response['expectedReturn']
     try:
         expected_return = float(expected_return.split("%")[0])
     except ValueError:
         expected_return = float((expected_return.split("%")[0]).split("-")[0])
+
+    horizon = data_store['investmentHorizon']
+    # horizon = float(horizon)
+    # horizon = Decimal(horizon)
+    # p = (composition/100) * float(data_store['amount'])
+    # fv = Decimal(p * (1 + (expected_return / 100))) ** horizon
+    # fv = float(fv)
     
-    horizon = data_store['investment_horizon']
-    horizon = float(horizon)       
-    p = (composition/100) * data_store['amount']
-    fv = p * (1 + (expected_return / 100) ) ** horizon
-    return round(fv, 2)
+    # horizon = Decimal(horizon)
+    p = (composition/100) * float(data_store['amount'])
+    fv = p * (1 + (expected_return / 100)) ** float(horizon)
+    # fv = float(fv)
+    return [p, round(fv, 2)]
+
+# def calculate_return(response, data_store):
+    from decimal import Decimal, getcontext
+
+    try:
+        composition = response['composition']
+        composition = float(composition.split("%")[0])
+    except KeyError:
+        return {'composition': 0, 'expectedReturn': 0}
+
+    expected_return = response['expectedReturn']
+    try:
+        expected_return = float(expected_return.split("%")[0])
+    except ValueError:
+        expected_return = float((expected_return.split("%")[0]).split("-")[0])
+
+    horizon = data_store['investmentHorizon']
+    horizon = Decimal(horizon)
+    p = (composition/100) * float(data_store['amount'])
+    fv = Decimal(p * (1 + (expected_return / 100))) ** horizon
+    fv = float(fv)
+    
+    return {'composition': composition, 'expectedReturn': expected_return, 'p': p, 'fv': round(fv, 2)}
+
+def process_responses(prompt, response):
+    while True:
+        response = prompt_response(prompt)
+        formatted_response=format_response(response)
+        keys = [i.keys() for i in formatted_response[:1]]
+        if len(keys[0]) == 6:
+            valid_list = formatted_response
+            break
+    return valid_list
+
 
 # capture and store the data received from post request
+# Configure CORS globally for all routes
+# Configure CORS globally to allow all headers (including 'Content-Type')
+# CORS(app, supports_credentials=False, origins="*", allow_headers="*", methods="*")
+
+
+# Arinze commented this out...
 @app.route('/gemini_service', methods=['POST'])
+# @app.route('/gemini_service', methods=['POST'])
 def store_data():
-    
+
     import os
-    import google.generativeai as genai # importing dependencies for the AI service
-
-    from dotenv import load_dotenv
-
-    load_dotenv()  # Load variables from .env file
+    import google.generativeai as genai  # importing dependencies for the AI service
 
     value = os.getenv('GOOGLE_API_KEY')
 
@@ -68,54 +149,90 @@ def store_data():
 
     model = genai.GenerativeModel('gemini-pro')
 
-
-
     global prompt_build, ai_response, formatted_response
 
+    print(request)
+
     data = request.get_json()
+    
+    # Arinze added this....
+     # Ensure that all required keys are present in the data dictionary
+    # required_keys = ['age', 'location', 'investmentKnowledge', 'investmentPurpose', 'investmentHorizon', 'riskTolerance', 'amount', 'currency']
+
+    # try:
+    #     for key in required_keys:
+    #         data_store[key] = data[key]
+    # except KeyError as e:
+    #     return jsonify({'error': f'Missing required key: {e.args[0]}'}, 400)
+
     # Store the data
     data_store['age'] = data['age']
     data_store['location'] = data['location']
-    data_store['investment_skill_level'] = data['investment_skill_level']
-    data_store['investment_purpose'] = data['investment_purpose']
-    data_store['investment_horizon'] = data['investment_horizon']
-    data_store['risk_tolerance'] = data['risk_tolerance']
+    data_store['investmentKnowledge'] = data['investmentKnowledge']
+    data_store['investmentPurpose'] = data['investmentPurpose']
+    data_store['investmentHorizon'] = data['investmentHorizon']
+    data_store['riskTolerance'] = data['riskTolerance']
     data_store['amount'] = data['amount']
     data_store['currency'] = data['currency']
 
-    
-
     # build prompt based on data fetched from the post request and persisted on data store
     prompt_build = get_prompt(
-            data_store['age'],
-            data_store['location'],
-            data_store['investment_skill_level'],
-            data_store['investment_purpose'],
-            data_store['investment_horizon'],
-            data_store['risk_tolerance'],
-            data_store['amount'],
-            data_store['currency']
-        )
-    
-    ## fetch response from ai service 
+        data_store['age'],
+        data_store['location'],
+        data_store['investmentKnowledge'],
+        data_store['investmentPurpose'],
+        data_store['investmentHorizon'],
+        data_store['riskTolerance'],
+        data_store['amount'],
+        data_store['currency']
+    )
+
+    # fetch response from ai service
     ai_response = prompt_response(prompt_build)
 
-
-    ## format ai response to required template 
+    # format ai response to required template
     formatted_response = format_response(ai_response)
+    
+    valid_response = process_responses(prompt_build, ai_response)
+    print(valid_response)
 
-    print([res.keys() for res in formatted_response])
+    # print([res.keys() for res in formatted_response])
     # Calculate return values for each item in formatted_response
-    return_values = [calculate_return(res, data_store) for res in formatted_response]
+    print("\n", "\n", "\n")
+    print([calculate_return(res, data_store) for res in valid_response])
+    
+    # principal_and_return_values = [
+    # (calculated_return.get(0, 0), calculated_return.get(1, 0))
+    # for calculated_return in [calculate_return(res, data_store) for res in formatted_response]
+    # ]
+    
+    # Arinze commented this out....
+    principal_and_return_values = [(calculate_return(res, data_store)[0],
+                                    calculate_return(res, data_store)[1])
+                                   for res in valid_response]
+
+    # Add principal amount to each item in formatted_response
+    prov_response = [{**rec, "principal": rv[0]}
+                     for rec, rv in zip(valid_response, principal_and_return_values)]
 
     # Add estimated return value to each item in formatted_response
-    final_response = [{**rec, "estimated_return_value": rv} for rec, rv in zip(formatted_response, return_values)]
+    final_response = [{**rec, "estimatedReturnValue": rv[1]}
+                      for rec, rv in zip(prov_response, principal_and_return_values)]
 
-    return jsonify({'recommendations': final_response}), 200
+    final_response = [
+        {**rec, "currency": get_currency_symbol(data_store['currency'])} for rec in final_response]
+    
+    response_store['recommendations'] = final_response
+
+    return jsonify({'status': 'success'}), 200
 
 
-
-
+@app.route('/gemini_response', methods=['GET'])
+def retrieve_data():
+    
+    return response_store['recommendations'], 200
+    
+    # return jsonify({'recommendations': response_store['recommendations']}), 200
 
 
 if __name__ == '__main__':
--- Flask_robo_response/services.py
+++ Flask_robo_response/services.py
@@ -1,5 +1,5 @@
 import os
-import google.generativeai as genai # importing dependencies for the AI service
+import google.generativeai as genai  # importing dependencies for the AI service
 
 value = os.getenv('GOOGLE_API_KEY')
 
@@ -8,38 +8,38 @@ genai.configure(api_key=value)
 model = genai.GenerativeModel('gemini-pro')
 
 prompt_template = """
-I am {} years old and I live in {}. I consider myself a {} in terms of investment and I wish to invest 
+I am {} years old and I live in {}. I consider myself a {} in terms of investment and I wish to invest
 for the purpose of {} over a {}-year horizon, and have a {} risk tolerance.
-I have a sum of {} in {} to invest. Which specific financial products ( including ticker and provider 
+I have a sum of {} in {} to invest. Which specific financial products ( including ticker and provider
 would a typical financial advisor recommend for investment given my circumstances? You may limit the number of recommendations to a minimum of five.
 You may also add an estimate of the expected return from each recommendation.
-Environmental factors are not important to me when I am investing. Which composition 
-(as a percentage) would he recommend for each financial product? 
-Each recommendation should be a proportion of '100%' of the amount to be invested. I will not consider your response personalized advice. 
-You may send the response in json format, let the key to the recommendations be given as recommendations, 
-and each key in the recommendation should be represented as ("financial_product", "ticker", "provider", "brief_description", "expected_return", "composition") and also ensure that composition comes last in the json. 
+Environmental factors are not important to me when I am investing. Which composition
+(as a percentage) would he recommend for each financial product?
+Each recommendation should be a proportion of '100%' of the amount to be invested. I will not consider your response personalized advice.
+You may send the response in json format, let the key to the recommendations be given as recommendations,
+and each key in the recommendation should be represented as ("financial_product", "ticker", "provider", "brief_description", "expected_return", "composition") and also ensure that composition comes last in the json.
 """
 
 
 def get_prompt(
-        age, 
-        location, 
-        investment_skill_level, 
-        investment_purpose, 
-        investment_horizon,
-        risk_tolerance,
+        age,
+        location,
+        investmentKnowledge,
+        investmentPurpose,
+        investmentHorizon,
+        riskTolerance,
         amount,
         currency
-        ):
-    prompt_template.format(age, 
-                            location, 
-                            investment_skill_level, 
-                            investment_purpose, 
-                            investment_horizon, 
-                            risk_tolerance, 
-                            amount, 
-                            currency)
-    
+):
+    prompt_template.format(age,
+                           location,
+                           investmentKnowledge,
+                           investmentPurpose,
+                           investmentHorizon,
+                           riskTolerance,
+                           amount,
+                           currency)
+
     return prompt_template
 
 
@@ -48,6 +48,7 @@ def prompt_response(prompt):
     response = model.generate_content(prompt)
 
     return response
+
 
 def format_response(response):
 
@@ -60,12 +61,13 @@ def format_response(response):
     raw = raw.replace("json", "")
 
     # print(raw) ############
-    #provisional list format 
-    prov_format = (raw.split('"recommendations": ')[1].split("]}")[0].split("[")[1]+",").split('},')
+    # provisional list format
+    prov_format = (raw.split('"recommendations": ')[1].split("]}")[
+                   0].split("[")[1]+",").split('},')
 
     prov_format = prov_format[:len(prov_format)-1]
 
-    # print("\n") 
+    # print("\n")
 
     # print(f"provisional format: {prov_format}") ############
 
@@ -79,9 +81,10 @@ def format_response(response):
         sublist = [a.split(": ")[1] for a in item]
         mlist.append(sublist)
 
-    response_list = [] # initialze an empty to store key value of each product recommendation 
-    keys = ["financial_product", "ticker", "provider", "brief_description", "expected_return", "composition"]
-    
+    response_list = []  # initialze an empty to store key value of each product recommendation
+    keys = ["financial_product", "ticker", "provider",
+            "brief_description", "expectedReturn", "composition"]
+
     # print(mlist)
     print("\n")
     for i in mlist:
@@ -90,6 +93,4 @@ def format_response(response):
 
     formatted_response = response_list
 
-    return formatted_response 
-
-
+    return formatted_response
