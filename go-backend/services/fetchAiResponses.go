b91748ffca788fa519e1c33c175830a06bce1e5e
Tonyrealzy <umehobiarinze2@gmail.com>
2025-04-29 17:49:56 +0100

    fix: Added additional models and services for fetchng data based on date range supplied.

--- go-backend/services/fetchAiResponses.go
+++ go-backend/services/fetchAiResponses.go
@@ -1,17 +1,17 @@
-package services
+// package services
 
-import (
-	"go-backend/config"
-	"go-backend/models"
-	"go-backend/utils"
+// import (
+// 	"go-backend/config"
+// 	"go-backend/models"
+// 	"go-backend/utils"
 
-	"encoding/json"
-	"fmt"
-	"log"
-	"time"
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// 	"time"
 
-	"gorm.io/datatypes"
-	"gorm.io/gorm"
-)
+// 	"gorm.io/datatypes"
+// 	"gorm.io/gorm"
+// )
 
-func FetchAIResponses(db *gorm.DB, req models.User) (*models.AIServiceResponse, error) {}
+// func FetchAIResponses(db *gorm.DB, req models.User) (*models.AIServiceResponse, error) {}
